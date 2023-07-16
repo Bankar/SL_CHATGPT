c5cc2e688e7f93344f991ac41344df03e70e174c
Bankar <ankitbankar@gmail.com>
2023-07-16 15:53:58 +0530

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -5,7 +5,7 @@ const { Configuration, OpenAIApi } = require("openai");
 
 const app = express();
 
-app.use(cors({origin: 'https://articulateusercontent.com'}));
+app.use(cors({origin: '*'}));
 app.use(express.json());
 
 const configuration = new Configuration({
